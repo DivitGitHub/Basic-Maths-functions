77808c9b1f891e6b612abeb2f0d2f8127113dbb9
DivitGitHub <divitngoc@gmail.com>
2017-04-07 21:35:48 +0100

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,13 +2,21 @@
 public class Factorial {
 	
 	public static void main(String[] args) {
-		System.out.println(factorial(7));
+		//Example: finds the factorial of 7
+		System.out.println(factorial(7)); //prints 5040
 	}
 	
+	/*
+	*Example 7*6*5*4*3*2
+	*@param finds the factorial of int
+	*@return returns long
+	*/
 	public static long factorial(int number) {
 		if (number == 1 || number == 0) {
 			return 1;
 		}
+		
+		//calls itself
 		return number * factorial(number-1);	
 	}
 }
