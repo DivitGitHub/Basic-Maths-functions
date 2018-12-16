3dd8e5a23ea391ee9b9a9659751475b009872ca4
Divit <divitngoc@gmail.com>
2018-12-16 00:46:50 +0000

    Update primeNumberChecker for better efficiency

--- PrimeNumberChecker.java
+++ PrimeNumberChecker.java
@@ -7,20 +7,24 @@ public class PrimeNumberChecker {
 	/*
 	 * returns true if the param is a prime
 	 * @param check the number
-	 * @return returns a boolean
+	 * @return returns boolean
 	 */
 	public static boolean primeNumberChecker(int number) {
-		int sqrtOfNumber = (int) Math.sqrt(number);	//No need to check for multiples after the square root of the param.
-
-		//edge case
 		if (number <= 1) {
+			return false;
+		} else if (number % 2 == 0) {
+			// If it's even, this is not a prime number 
 			return false;
 		}
 		
-		//checks either the number could be divided...
-		//starts iterating from 2 since all numbers could be divided by 1;
-		for (int i = 2; i <= sqrtOfNumber; i++) {
+		// No need to check for multiples after the square root of the param.
+		int sqrtOfNumber = (int) Math.sqrt(number);
+		
+		// Checks either the number could be divided...
+		// Starts iterating odd numbers
+		for (int i = 3; i <= sqrtOfNumber; i += 2) {
 			if (number % i == 0) {
+				// If it could be divided, it's not a prime number
 				return false;
 			}
 		}
