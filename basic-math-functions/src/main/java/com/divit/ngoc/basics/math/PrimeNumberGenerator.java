c1eaba8420f134e660aec59e82aa27c1a0c032e4
Divit <divitngoc@gmail.com>
2019-01-08 19:52:01 +0000

    Update prime generate to be more efficient

--- basic-math-functions/src/main/java/com/divit/ngoc/basics/math/PrimeNumberGenerator.java
+++ basic-math-functions/src/main/java/com/divit/ngoc/basics/math/PrimeNumberGenerator.java
@@ -11,32 +11,39 @@ public class PrimeNumberGenerator {
 	 *
 	 * @param number
 	 * 
-	 * @return An ArrayList, it will return an empty list if there's no prime
-	 * numbers
+	 * @return List of prime numbers, otherwise it'll return an empty list
+	 * This can never be null.
 	 */
 	public List<Integer> generate(int number) {
-		List<Integer> list = new ArrayList<Integer>();
+		if (number <= 1) {
+			return new ArrayList<>();
+		}
 
 		// To flag if the number is a prime number or not
-		boolean isPrime;
+		boolean isPrime = false;
 
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(2);
 		// Check every number starting from 2 to see if it's a prime or not...
-		for (int i = 2; i <= number; i++) {
+		for (int i = 3; i <= number; i++) {
 
 			// No need to check for multiples after the square root of the number we're
 			// checking...
 			int sqrtOfi = (int) Math.sqrt(i);
 			isPrime = true;
 
-			// This checks if the int i can be divided(with no remainder)...
 			// If it can be divided, it sets isPrime to false and exits that loop.
-			for (int k = 2; k <= sqrtOfi; k++) {
-				if (i % k == 0) {
-					isPrime = false;
-					break;
+			if (i % 2 == 0) {
+				isPrime = false;
+			}
+			else if {
+				for (int k = 3; k <= sqrtOfi; k += 2) {
+					if (i % k == 0) {
+						isPrime = false;
+						break;
+					}
 				}
 			}
-
 			if (isPrime) {
 				list.add(i);
 			}
